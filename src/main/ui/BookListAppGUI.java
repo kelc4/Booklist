fb490ee0fd229a34b54717cd29cf5bb633e3f33f
Kelsey Chua <77289991+kelc4@users.noreply.github.com>
2021-11-09 23:35:48 -0800

    Made a JFrame

---
+++ src/main/ui/BookListAppGUI.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javax.swing.*;
+
+public class BookListAppGUI {
+
+    public void runGUI() {
+        JFrame frame = new JFrame();
+        frame.setTitle("BookList Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes program stop running after closing frame
+        frame.setResizable(false); //prevents frame from being resized
+        frame.setSize(420, 420); //x, y dimensions of frame
+        frame.setVisible(true); //makes frame visible
+
+
+
+    }
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,14 +1,18 @@
 package ui;
 
+import model.Book;
+
 import java.io.FileNotFoundException;
 
 //Runs the app
 public class Main {
     public static void main(String[] args) {
-        try {
-            new BookListApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run application: file not found");
-        }
+//        try {
+//            new BookListApp();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Unable to run application: file not found");
+//        }
+        BookListAppGUI g = new BookListAppGUI();
+        g.runGUI();
     }
 }
